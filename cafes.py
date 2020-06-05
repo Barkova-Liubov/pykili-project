2d0e942a92753e2057116934ed567175dbcd5d5d
Barkova-Liubov <55841178+Barkova-Liubov@users.noreply.github.com>
2020-06-05 16:13:36 +0300

    Add files via upload

--- cafes.py
+++ cafes.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, redirect, url_for, request
 from csv import DictReader
-
-ALL_CAFES = list(DictReader(open('project.csv', encoding = 'utf-8')))
-for cafe in ALL_CAFES:
-        for key in cafe:
-            if cafe [key] == 'нет':
-                cafe[key] = None
+from random import choice
 
 KEY_SET = {'название','открытие','закрытие','адрес','добираться'}
 
 def filter(coffee, price, time):
+    all_cafes = list( DictReader( open( 'project.csv', encoding='utf-8' ) ) )
+    for cafe in all_cafes:
+        for key in cafe:
+            if cafe[key] == 'нет':
+                cafe[key] = None
     some_cafes = []
     less_cafes = []
     required_cafes = []
     if not coffee == 'любой':
-        for cafe in ALL_CAFES:
+        for cafe in all_cafes:
             for key in cafe:
                 if key == coffee:
                     if not cafe[key] == None:
@@ -26,16 +26,16 @@ def filter(coffee, price, time):
             if int(cafe['добираться']) <= int(time):
                 required_cafes.append(cafe)
     else:
-        for cafe in ALL_CAFES:
+        for cafe in all_cafes:
             cafe['любой'] = None
-        for cafe in ALL_CAFES:
+        for cafe in all_cafes:
             for key in cafe:
                 if key == 'эспрессо' or key == 'латте' or key == 'раф' or key == 'американо' or key == 'капучино' or key == 'флэт уайт' or key == 'мокко':
                     if not cafe[key] == None:
                         if int(cafe[key]) <= int( price ):
                             cafe['любой'] = 'да'
 
-        for cafe in ALL_CAFES:
+        for cafe in all_cafes:
             if cafe['любой'] == 'да':
                 less_cafes.append(cafe)
 
@@ -59,35 +59,49 @@ def list_creator(required_cafes, coffee):
             required_cafe.pop('любой')
             cafes_for_html.append(required_cafe)
 
-    for Di in cafes_for_html:
-        for key in Di:
-            if key == 'добираться':
-                if Di[key].endswith( '2' ) and not Di[key].startswith( '1' ) or Di[key].endswith( '3' ) and not \
-                Di[key].startswith( '1' ) or Di[key].endswith( '4' ) and not Di[key].startswith( '1' ):
-                    Di[key] = Di[key] + ' минуты'
-                elif Di[key].endswith( '1' ) and not Di[key].startswith( '1' ):
-                    Di[key] = Di[key] + ' минуту'
-                else:
-                    Di[key] = Di[key] + ' минут'
 
     for Di in cafes_for_html:
         for key in Di:
             if key == 'эспрессо' or key == 'латте' or key == 'раф' or key == 'американо' or key == 'капучино'\
             or key == 'флэт уайт' or key == 'мокко':
-                if not Di[key] == None:
-                    if Di[key].endswith( '2' ) and not Di[key].startswith( '1' ) or Di[key].endswith('3' ) \
-                    and not Di[key].startswith( '1' ) or Di[key].endswith( '4' ) and not Di[key].startswith('1' ):
-                        Di[key] = Di[key] + ' рубля'
-                    elif Di[key].endswith( '1' ) and not Di[key].startswith( '1' ):
-                        Di[key] = Di[key] + ' рубль'
-                    else:
-                        Di[key] = Di[key] + ' рублей'
-                else:
+                if  Di[key] == None:
                     Di[key] = 'нет'
     KEY_SET.remove(coffee)
     return cafes_for_html
 
+def chooser(cafes_for_html):
+    fin_dict = choice(cafes_for_html)
+    return fin_dict
+
+def comments (fin_dict):
+    for key in fin_dict:
+        if key == 'добираться':
+            if fin_dict[key].endswith( '2' ) and not fin_dict[key].startswith( '1' ) or fin_dict[key].endswith(
+                    '3' ) and not \
+                    fin_dict[key].startswith( '1' ) or fin_dict[key].endswith( '4' ) and not fin_dict[key].startswith(
+                '1' ):
+                fin_dict[key] = fin_dict[key] + ' минуты'
+            elif fin_dict[key].endswith( '1' ) and not fin_dict[key].startswith( '1' ):
+                fin_dict[key] = fin_dict[key] + ' минуту'
+            else:
+                fin_dict[key] = fin_dict[key] + ' минут'
+
+    for key in fin_dict:
+        if key == 'эспрессо' or key == 'латте' or key == 'раф' or key == 'американо' or key == 'капучино' \
+            or key == 'флэт уайт' or key == 'мокко':
+            if not fin_dict[key] == None:
+                if fin_dict[key].endswith( '2' ) and not fin_dict[key].startswith( '1' ) or fin_dict[key].endswith( '3' ) \
+                    and not fin_dict[key].startswith( '1' ) or fin_dict[key].endswith( '4' ) and not fin_dict[
+                key].startswith \
+                        ( '1' ):
+                    fin_dict[key] = fin_dict[key] + ' рубля'
+                elif fin_dict[key].endswith( '1' ) and not fin_dict[key].startswith( '1' ):
+                    fin_dict[key] = fin_dict[key] + ' рубль'
+                else:
+                    fin_dict[key] = fin_dict[key] + ' рублей'
+    return fin_dict
 app = Flask(__name__)
+
 @app.route('/')
 def page_with_questions():
     return render_template('questions.html')
@@ -100,9 +114,11 @@ def where_to_go():
     coffee,price,time = data.split('\n')
     required_cafes = filter(coffee,price,time)
     cafes_for_html = list_creator(required_cafes,coffee)
+    fin_dict = chooser(cafes_for_html)
+    fin_dict = comments(fin_dict)
     if cafes_for_html == []:
         sad_news = 'К сожалению, мы не нашли для вас подходящих кофеен'
-    return render_template('cafes.html', places = cafes_for_html, sad_news = sad_news)
+    return render_template('cafes.html', place = fin_dict, sad_news = sad_news)
 
 @app.route('/process', methods=['get'])
 def answer_process():
